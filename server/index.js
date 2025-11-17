b08d47fa1c4aba703f4193064d12d45649dc022c
harshitazzz <harshitamaheshwari321@gmail.com>
2025-11-18 02:54:53 +0530

    package.json changes for server

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 const express=require('express');
 const cors= require('cors');
 const app= express();
-const bodyParser = require('body-parser');
 const AuthRouter= require('./Routes/AuthRouter');
 
 require('dotenv').config();
